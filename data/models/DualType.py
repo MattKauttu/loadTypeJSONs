c4d96200abefc42645525cacdf4cc7aeeb34b3ab
Matt Kauttu <m.a.kauttu95@gmail.com>
2022-10-20 07:57:28 -0400

    Begin implementing get_dual_types()

---
+++ data/models/DualType.py
@@ -0,0 +1,14 @@
+
+class DualType:
+
+    # name
+    # defending relationships
+    #     dictionary
+    #         type - dmgModifier
+
+    def __init__(self, name, coverage, defending):
+        self.name = name
+        self.coverage = coverage
+        self.defending = defending
+
+    pass
--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 from loadSingleTypeJSON import load_json
+from itertools import combinations
 
 
-def generateDualTypes(base_types):
-
-
+def generate_dual_type(combo):
     pass
+
+
+def get_dual_types(base_types):
+    type_combinations = get_base_type_combinations(base_types)
+    dual_types = list()
+
+    for combo in type_combinations:
+        dual_type = generate_dual_type(combo)
+        dual_types.append(dual_type)
+
+    return dual_types
+
+
+def get_base_type_combinations(base_types):
+    type_combinations = list()
+
+    for n in range(1, 3):
+        type_combinations += list(combinations(base_types, n))
+
+    return type_combinations
 
 
 def main():
     base_types = load_json()
 
-    dual_types = generateDualTypes(base_types)
+    dual_types = get_dual_types(base_types)
 
     pass
 
